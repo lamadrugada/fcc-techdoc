7a43605442c8e6df8906b420dae7cb07bce11019
Julia <julia.bula.net@gmail.com>
2019-07-17 12:05:43 +0200

    Added toggled menu on smallest devices

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    //MARK.JS
     const SEARCH_INPUT = document.querySelector('#searchinput');
     const SEARCH_BUTTON = document.querySelector('#searchbutton');
     const CONTEXT = document.querySelectorAll('.main-section');
-
     const INSTANCE = new Mark(CONTEXT);
 
     let getMarked = () => {
@@ -15,4 +15,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     SEARCH_BUTTON.addEventListener('click', getMarked);
     
+
+    //HAMBURGER
+    const TOGGLER = document.querySelector('.toggler');
+    const NAVIGATION_LIST = document.querySelector('.navList');
+    
+    let toggleMenu = () => {
+        NAVIGATION_LIST.classList.toggle('show');
+    }
+
+    TOGGLER.addEventListener('click', toggleMenu);
 });
